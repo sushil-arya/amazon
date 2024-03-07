a8a5cb483a26d34c8f420610366f1c804c569649
sushil-arya <sushilarya888@gmail.com>
2024-03-07 16:24:43 +0530

    Day-1

---
+++ amazon/src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+function App() {
+  return(
+    <div>
+      <header>
+        
+      </header>
+
+      <div className="s_main">
+
+      </div>
+
+      <footer>
+
+      </footer>
+    </div>
+  );
+}
+
+export default App;
