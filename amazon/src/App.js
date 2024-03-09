1250e6a2207ab5fa54d13b035b188fbe85422282
sushil-arya <sushilarya888@gmail.com>
2024-03-09 17:46:14 +0530

    Day-2

--- amazon/src/App.js
+++ amazon/src/App.js
@@ -1,19 +1,75 @@
+import "./App.css";
 
-import './App.css';
 function App() {
-  return(
+  return (
     <div>
       <header>
-        
+        <div className="s_header_top"></div>
+        <div className="s_header_bottom"></div>
       </header>
 
       <div className="s_main">
+        <div className="s_main1 position-relative">
+          <div id="carouselExample" class="carousel slide">
+            <div className="carousel-inner">
+              <div className="carousel-item active">
+                <img
+                  src="./images/slider/image_1.jpg"
+                  className="d-block w-100"
+                  alt="image1"
+                />
+              </div>
+              <div className="carousel-item">
+                <img src="./images/slider/image_2.jpg" className="d-block w-100" alt="..." />
+              </div>
+              <div className="carousel-item">
+                <img src="./images/slider/image_3.jpg" className="d-block w-100" alt="..." />
+              </div>
+              <div className="carousel-item">
+                <img src="./images/slider/image_4.jpg" className="d-block w-100" alt="..." />
+              </div>
+              <div className="carousel-item">
+                <img src="./images/slider/image_5.jpg" className="d-block w-100" alt="..." />
+              </div>
+              <div className="carousel-item">
+                <img src="./images/slider/image_6.jpg" className="d-block w-100" alt="..." />
+              </div>
+              <div className="carousel-item">
+                <img src="./images/slider/image_7.jpg" className="d-block w-100" alt="..." />
+              </div>
+            </div>
+            <button
+              className="carousel-control-prev h-50"
+              type="button"
+              data-bs-target="#carouselExample"
+              data-bs-slide="prev"
+            >
+              <span
+                className="carousel-control-prev-icon"
+                aria-hidden="true">
+              </span>
+              <span className="visually-hidden">Previous</span>
+            </button>
+            <button
+              className="carousel-control-next h-50"
+              type="button"
+              data-bs-target="#carouselExample"
+              data-bs-slide="next">
+              <span
+                class="carousel-control-next-icon"
+                aria-hidden="true">
+              </span>
+              <span className="visually-hidden">Next</span>
+            </button>
+          </div>
+        </div>
 
+        <div className="s_main2 position-absolute w-100">
+
+        </div>
       </div>
 
-      <footer>
-
-      </footer>
+      <footer></footer>
     </div>
   );
 }
